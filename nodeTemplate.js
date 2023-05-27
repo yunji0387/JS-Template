84deccc614dffee402a0935ed5818d05b8a7c36d
Yun JI <59977078+yunji0387@users.noreply.github.com>
2023-05-27 16:04:52 -0500

    Add NodeJs template file

---
+++ nodeTemplate.js
@@ -0,0 +1,19 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.get("/", function(req, res){
+    res.send("Get - Hello World");
+});
+
+app.post("/failure", function(req, res){
+    res.send("Post - Hello World"); 
+ });
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000");
+});
+
