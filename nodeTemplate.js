ecbb91daadb79d356e7d4444658c2a939c5852b8
Yun JI <59977078+yunji0387@users.noreply.github.com>
2023-05-29 13:27:50 -0500

    Update nodeTemplate.js

--- nodeTemplate.js
+++ nodeTemplate.js
@@ -2,9 +2,12 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
 
 const app = express();
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public")); //for accessing the standar file structure, example : public/css
 
 app.get("/", function(req, res){
     res.send("Get - Hello World");
