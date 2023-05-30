9bc954f3ad26f50df8f2a902c80adb22bfddf047
Yun JI <59977078+yunji0387@users.noreply.github.com>
2023-05-30 13:52:01 -0500

    Update nodeTemplate.js

--- nodeTemplate.js
+++ nodeTemplate.js
@@ -1,23 +1,24 @@
 //jshint esversion:6
 
+//importing libraries
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
-const app = express();
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended:true}));
+const app = express(); //creating express application
+app.set('view engine', 'ejs'); //setting view engine to ejs
+app.use(bodyParser.urlencoded({extended:true})); // Middleware for parsing URL-encoded request bodies
 app.use(express.static("public")); //for accessing the standar file structure, example : public/css
 
-app.get("/", function(req, res){
+app.get("/", function(req, res){ // Handling GET request to the root route ("/")
     res.send("Get - Hello World");
 });
 
-app.post("/", function(req, res){
+app.post("/", function(req, res){ // Handling POST request to the root route ("/")
     res.send("Post - Hello World"); 
  });
 
-app.listen(3000, function(){
+app.listen(3000, function(){ // Starting the server and listening on port 3000
     console.log("Server started on port 3000");
 });
 
